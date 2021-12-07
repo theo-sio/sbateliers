65782fb553724fc834032b37a799aff9798cdff4
theo-sio <90323842+theo-sio@users.noreply.github.com>
2021-12-07 10:54:00 +0100

    Delete sbateliers.sql

--- sbateliers.sql
+++
@@ -1,2 +0,0 @@
-insert into client(civilite,nom,prenom,date_de_naissance,adresse_electronique,mot_de_passe,adresse_postale,code_postale,ville,numero_de_mobile) values('$civilite', '$nom', '$prenom', '$date_de_naissance', '$adresse_electronique', '$mdp','$adresse_postale',  $code_postale, '$ville', $numero_mobile)" ;
-		$bd->exec($sql);
